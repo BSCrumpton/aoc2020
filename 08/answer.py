11c45b0c8e43af953adde233caa35e089536708c
Crumpton, Bryan S <saul.crumpton@gatech.edu>
2020-12-08 00:51:54 -0500

    fixed code seperation for part 1 and 2

--- 08/answer.py
+++ 08/answer.py
@@ -2,26 +2,23 @@ with open('input', newline='\r\n') as inputfile:
     input=inputfile.read().split('\r\n')
 
 accumulator=0
+ranlines=[]
 noOpsAndJmps=[]
 for index,row in enumerate(input):
     temp=row.split()[0]
     if(temp=="nop" or temp=="jmp"):
         noOpsAndJmps.append(index)
-# print(noOpsAndJmps)
 
-def doRow(rownumber,accumulator,fixed,ranlines):
+def doRow(rownumber,accumulator,fixed):
     # print(rownumber, ' ',input[rownumber]," | ",accumulator)
-    # print("fixed: ",fixed)
-    if(rownumber>len(input)-1):
-        # print(fixed)
+    if(rownumber>len(input)):
         print(accumulator)
         return False
     if (rownumber in ranlines):
-        # print(accumulator)
+        print(accumulator)
         return True
     tokens = input[rownumber].split()
     if rownumber == fixed:
-        # print(fixed)
         if(tokens[0]=="nop"):
             tokens[0]="jmp"
         else:
@@ -29,25 +26,11 @@ def doRow(rownumber,accumulator,fixed,ranlines):
     ranlines.append(rownumber)
     if(tokens[0]=="acc"):
         accumulator=accumulator+int(tokens[1])
-        return doRow(rownumber + 1, accumulator,fixed,ranlines)
+        doRow(rownumber + 1, accumulator,fixed)
     elif(tokens[0]=="jmp"):
-        return doRow(rownumber+int(tokens[1]), accumulator,fixed,ranlines)
+        doRow(rownumber+int(tokens[1]), accumulator,fixed)
     else:
-        return doRow(rownumber+1,accumulator,fixed,ranlines)
+        doRow(rownumber+1,accumulator,fixed)
 
 
-inLoop=True
-
-while (inLoop):
-    # inLoop=doRow(0,0,-1)
-    # print(inLoop)
-    ranlines=[]
-    if(inLoop):
-        inLoop=doRow(0,0,noOpsAndJmps[0],ranlines)
-    if(inLoop):
-        # print("didn't solve")
-        noOpsAndJmps=noOpsAndJmps[1:]
-        # print(noOpsAndJmps)
-    else:
-        break
-# 432 not correct
+doRow(0,0,-1)
--- 08/answerPart2.py
+++ 08/answerPart2.py
@@ -1,57 +1,53 @@
 with open('input', newline='\r\n') as inputfile:
     input=inputfile.read().split('\r\n')
-contains={}
-containsGold={}
-baseBags=[]
-for rule in input:
-    tokens=rule.split()
-    location=1
-    temp={}
-    for index,word in enumerate(tokens):
-        word=word.strip()
-        # print(index,' ',word)
-        if(word=="bags," or word=="bag," or word=="bag." or word=="bags."):
-            if(index==2):
-                continue
-            if(tokens[index-2]=="no"):
-                baseBags.append(tokens[0]+" "+tokens[1])
-                continue
-            temp[tokens[index-2]+" "+tokens[index-1]]=int(tokens[index-3])
-    if(not temp):
-        continue
-    contains[tokens[0]+" "+tokens[1]]=temp
-# print (baseBags)
-# print(contains)
-# print(contains)
-#
-def recurGetBags(bagType):
 
-    tempContainedBags=0
-    if(bagType in baseBags):
-        return 0
-    # print(bagType)
-    # print(bagType,' ',contains[bagType])
-    for recurkey, recurvalue in contains[bagType].items():
-        temp=recurGetBags(recurkey)
-        # print(bagType,' ' ,temp,'*',recurvalue)
-        temp=temp+1
-        tempContainedBags = tempContainedBags + (recurvalue * temp )
-    # print(tempContainedBags)
-    return tempContainedBags
+accumulator=0
+noOpsAndJmps=[]
+for index,row in enumerate(input):
+    temp=row.split()[0]
+    if(temp=="nop" or temp=="jmp"):
+        noOpsAndJmps.append(index)
+# print(noOpsAndJmps)
+
+def doRow(rownumber,accumulator,fixed,ranlines):
+    # print(rownumber, ' ',input[rownumber]," | ",accumulator)
+    # print("fixed: ",fixed)
+    if(rownumber>len(input)-1):
+        # print(fixed)
+        print(accumulator)
+        return False
+    if (rownumber in ranlines):
+        # print(accumulator)
+        return True
+    tokens = input[rownumber].split()
+    if rownumber == fixed:
+        # print(fixed)
+        if(tokens[0]=="nop"):
+            tokens[0]="jmp"
+        else:
+            tokens[0]="nop"
+    ranlines.append(rownumber)
+    if(tokens[0]=="acc"):
+        accumulator=accumulator+int(tokens[1])
+        return doRow(rownumber + 1, accumulator,fixed,ranlines)
+    elif(tokens[0]=="jmp"):
+        return doRow(rownumber+int(tokens[1]), accumulator,fixed,ranlines)
+    else:
+        return doRow(rownumber+1,accumulator,fixed,ranlines)
 
 
-containedBags=0
-for key,value in contains["shiny gold"].items():
-    # print(key,' ',value)
-    containedBags=containedBags+(value*recurGetBags(key))+value
-    # print(containedBags)
-    # break
+inLoop=True
 
-
-# print(containsGold)
-# break}
-print(containedBags)
-# 145043 too low
-# 145057 not correct
-# 202270 not correct
-172246
+while (inLoop):
+    # inLoop=doRow(0,0,-1)
+    # print(inLoop)
+    ranlines=[]
+    if(inLoop):
+        inLoop=doRow(0,0,noOpsAndJmps[0],ranlines)
+    if(inLoop):
+        # print("didn't solve")
+        noOpsAndJmps=noOpsAndJmps[1:]
+        # print(noOpsAndJmps)
+    else:
+        break
+# 432 not correct
