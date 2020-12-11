9e64e628fa1a68e321bbe744ff034a74d7cc50f1
Crumpton, Bryan S <saul.crumpton@gatech.edu>
2020-12-11 01:19:16 -0500

    done with day 11

---
+++ 11/answer.py
@@ -0,0 +1,64 @@
+import copy
+with open('input', newline='\r\n') as inputfile:
+    input=inputfile.read().split('\r\n')
+
+input = [list(i) for i in input]
+
+adjacency=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+
+def printMap(input):
+    for row in input:
+        print(row)
+    print()
+# printMap(input)
+
+
+def shouldSwap(row,column,map):
+    type=map[row][column]
+    countFilled=0
+    for seat in adjacency:
+        seatRow=row+seat[0]
+        seatColumn=column+seat[1]
+        if(seatRow<0 or seatColumn<0 ):
+            continue
+        try:
+            # if(row==78 and column==0):
+            #     print(seatRow,' ', seatColumn, ' ',map[seatRow][seatColumn])
+            if(map[seatRow][seatColumn]=="#"):
+                countFilled=countFilled+1
+        except Exception as e:
+            # print(e)
+            continue
+    if(type=="L" and countFilled==0):
+        return True
+    elif(type=="#" and countFilled>3):
+        return True
+    else:
+        return False
+
+for i in range(200):
+    tempMap=copy.deepcopy(input)
+    for rowindex,row in enumerate(input):
+        for slotindex,slot in enumerate(row):
+            # printMap(input)
+            if(shouldSwap(rowindex,slotindex,input)):
+                if(input[rowindex][slotindex]=="L"):
+                    tempMap[rowindex][slotindex] = "#"
+                else:
+                    tempMap[rowindex][slotindex] = "L"
+        # break
+    input=copy.deepcopy(tempMap)
+
+
+printMap(input)
+
+filledCount=0
+for row in input:
+    for column in row:
+        if(column=="#"):
+            filledCount=filledCount+1
+print(filledCount)
+
+#not 2003
+# not 2236
+# not 703
